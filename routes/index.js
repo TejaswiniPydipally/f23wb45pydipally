37d052d3e53c8906311f79510f22ba86051cfded
TejaswiniPydipally <143117469+TejaswiniPydipally@users.noreply.github.com>
2023-10-26 23:56:40 -0500

    made changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express f23wb45pydipally' });
 });
 
 module.exports = router;
